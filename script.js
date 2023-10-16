4022ad7c29277baec0b5f11f2621f443e50fc197
Latisha <latisha.carvalho02@hotmail.com>
2023-10-16 12:12:34 +0200

    Ende Lektion

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+function deleteText () {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+function incrementCounter () {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter () {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num -1
+  numberElement.textContent = num
+}
+
+function resetCounter () {
+
+}
+
+function writeText () {
+  
+}
